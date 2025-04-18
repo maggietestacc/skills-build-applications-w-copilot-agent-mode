8f370db9690e117feb59fa6e8fa96ed950af2fe1
maggietestacc <maggietestacc@hotmail.com>
2025-04-18 17:33:44 +0000

    Fix database engine configuration and successfully populate test data

--- octofit_tracker_app/models.py
+++ octofit_tracker_app/models.py
@@ -1,23 +1,29 @@
+from djongo.models import ObjectIdField
 from django.db import models
 
 class User(models.Model):
+    id = ObjectIdField(primary_key=True)
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
 
 class Team(models.Model):
+    id = ObjectIdField(primary_key=True)
     name = models.CharField(max_length=255)
     members = models.ManyToManyField(User)
 
 class Activity(models.Model):
+    id = ObjectIdField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     type = models.CharField(max_length=255)
     duration = models.IntegerField()
 
 class Leaderboard(models.Model):
+    id = ObjectIdField(primary_key=True)
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     score = models.IntegerField()
 
 class Workout(models.Model):
+    id = ObjectIdField(primary_key=True)
     name = models.CharField(max_length=255)
     description = models.TextField()
